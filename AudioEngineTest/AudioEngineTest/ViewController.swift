6b9fb4a3f749ad1b1255b42e8f1aee51e88448ac
Arnav Anshul <aanshul@amazon.com>
2019-10-29 18:27:04 -0400

    Added default app delegate methods to Xcode 11 project

---
+++ AudioEngineTest/AudioEngineTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AudioEngineTest
+//
+//  Created by Anshul, Arnav on 10/29/19.
+//  Copyright © 2019 Anshul, Arnav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
