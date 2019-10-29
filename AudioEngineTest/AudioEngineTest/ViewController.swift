c96478ee9e6addc2d24b598a71c19146f830ae88
Arnav Anshul <aanshul@amazon.com>
2019-10-29 19:36:34 -0400

    Added local audio assets

--- AudioEngineTest/AudioEngineTest/ViewController.swift
+++ AudioEngineTest/AudioEngineTest/ViewController.swift
@@ -8,13 +8,36 @@
 
 import UIKit
 
+enum Format: String {
+    case mp3
+    case aac
+    case aiff
+    case flac
+}
+
+
 class ViewController: UIViewController {
     @IBOutlet weak var playPauseButton: UIButton!
     @IBOutlet weak var playbackProgressView: UIProgressView!
 
+    let fileName = "ff-16b-2c-44100hz"
+
+    static let playLabelTitle = "PLAY"
+    static let pauseLabelTitle = "PAUSE"
+
+    let streamingAudio = true
+    let playFormat = Format.aac
+
+    var progressMonitor: Timer?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        guard let localFileURL = Bundle.main.url(forResource: fileName, withExtension: playFormat.rawValue) else {
+            playPauseButton.isHidden = true
+            return
+        }
 
         playbackProgressView.progress = 0.0
     }
